31196897dfde383be9e61042d3c3a3ef38357e06
James Jones <jejones3141@gmail.com>
2020-01-19 19:40:01 -0600

    refactored code and added text file with still-open questions

--- pencil.py
+++ pencil.py
@@ -5,20 +5,24 @@ class Pencil:
         self.eraserDurability = eraserDurability
         self.text = ''
         self.erasedPos = None
+    
+    def _writeChar(self, c):
+        wear = 0 if c.isspace() else 2 if c.isupper() else 1
+        c = ' ' if self.pointDurability < wear else c
+        self.pointDurability = max(self.pointDurability - wear, 0)
+        return c
+    
+    def _insert(self, prefixLen, text, suffixPos):
+        self.text = self.text[:prefixLen] + text + self.text[suffixPos:]
 
     def write(self, text):
         for c in text:
-            wear = 0 if c.isspace() else 2 if c.isupper() else 1
-            self.text += ' ' if self.pointDurability < wear else c
-            self.pointDurability = max(self.pointDurability - wear, 0)
+            self.text += self._writeChar(c)
             
     def sharpen(self):
         if self.length > 0:
             self.pointDurability = self.initPointDurability
             self.length -= 1
-            
-    def _eraseWear(c):
-        return 
             
     def erase(self, text):
         tPos = self.text.rfind(text)
@@ -30,24 +34,21 @@ class Pencil:
                 break
             numErased += 1
             self.eraserDurability -= 0 if c.isspace() else 1
+        
         suffixPos = tPos + len(text)
         self.erasedPos = suffixPos - numErased
-        self.text = (self.text[:self.erasedPos] + (numErased * ' ') +
-                     self.text[suffixPos:])
+        self._insert(self.erasedPos, numErased * ' ', suffixPos)
 
     def overwrite(self, text):
         if self.erasedPos is not None:
             replacement = ''
             selfTextPos = self.erasedPos
             for c in text:
-                wear = 0 if c.isspace() else 2 if c.isupper() else 1
-                replacement += (     '@' if self.text[selfTextPos] != ' '
-                                else ' ' if self.pointDurability < wear else c)
-                self.pointDurability = max(self.pointDurability - wear, 0)
+                rc = c if self.text[selfTextPos].isspace() else '@'
+                replacement += self._writeChar(rc)
                 selfTextPos += 1
             suffixPos = self.erasedPos + len(text)
-            self.text = self.text[:self.erasedPos] + replacement + self.text[suffixPos:]
-            self.erasedPos = None
+            self._insert(self.erasedPos, replacement, suffixPos)
 
     def getText(self):
         return self.text
